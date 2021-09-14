ac5da1939f141daa71b089d94e047fff3a8bcdd9
manlowtech <charumbiramanlow20@gmail.com>
2021-09-14 23:34:33 +0200

    jxnnxn

--- src/mernmodules/components/AdminTemplate.js
+++ src/mernmodules/components/AdminTemplate.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import { Admin,Resource } from "react-admin";
-import { createHashHistory } from 'history';
-import restProvider from "ra-data-simple-rest";
-function Hello(){
-    return "Dashboard Ready"
-}
-const history = createHashHistory();
+
 function AdminTemplate() {
     return (
-        <Admin   history={history} dataProvider={restProvider('http://localhost:5000')}>
-            <Resource name="posts" create={Hello} />
-        </Admin>
+        <div>
+            
+        </div>
     )
 }
 
