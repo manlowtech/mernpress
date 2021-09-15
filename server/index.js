3d6ab5b8627dc0ea613e6a238e3be992e3b35c5e
manlowtech <charumbiramanlow20@gmail.com>
2021-09-15 15:23:51 +0200

    fhdhdnddd

--- server/index.js
+++ server/index.js
@@ -20,11 +20,11 @@ if(process.env.MONGO_URL && process.env.MONGO_URL !== ""){
 }
 app.get('*', (req, res) => res.status(200).send({
     message: 'Welcome to  the beginning of nothingness.',
-}));/*
+}));
 if(process.env.NODE_ENV == "production"){
 
 // ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "../", "build")))
 
 // ...
 // Right before your app.listen(), add this:
@@ -32,7 +32,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-} */
+} 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
