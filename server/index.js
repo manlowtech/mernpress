1bc9d025890f5e270250376cea0b10e8bcd36665
manlowtech <charumbiramanlow20@gmail.com>
2021-09-15 15:16:07 +0200

    fhdhdnddd

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ if(process.env.MONGO_URL && process.env.MONGO_URL !== ""){
 }
 app.get('*', (req, res) => res.status(200).send({
     message: 'Welcome to  the beginning of nothingness.',
-}));
+}));/*
 if(process.env.NODE_ENV == "production"){
 
 // ... other app.use middleware 
@@ -32,7 +32,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-}
+} */
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
