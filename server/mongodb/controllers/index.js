ab627e59dc0c6563669bd231ee5f2d820c43b3c8
manlowtech <charumbiramanlow20@gmail.com>
2021-09-15 20:20:28 +0200

    fhdhdngdgdggdddd

--- server/mongodb/controllers/index.js
+++ server/mongodb/controllers/index.js
@@ -1,6 +1,6 @@
 const Posts = require('./Posts');
 const Cat = require('./AddCategory');
-const Configs = require('./configs');
+const Configs = require('./Configs');
 const Menus = require('./MenuPages');
 const Users = require('./UsersController');
 module.exports = {
